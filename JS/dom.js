9276174bb9f89c5b6096b056597704a4002a8596
nurnewas <nurnewasjr@gmail.com>
2024-11-27 11:27:39 -0600

    inner text

--- JS/dom.js
+++ JS/dom.js
@@ -2,3 +2,5 @@
 // document.write("hello world");
 // console.log(document);
 document.getElementsByTagName('ul').append
+const pragraph = document.getElementById('pragraph');
+console.log(pragraph);
