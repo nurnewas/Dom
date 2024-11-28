d4983aa43ee368a7a7c4a37ebfc4c8179d440579
nurnewas <nurnewasjr@gmail.com>
2024-11-28 04:27:37 -0600

    find Section

--- JS/dom.js
+++ JS/dom.js
@@ -1,6 +1,4 @@
-// console.log("hello world");
-// document.write("hello world");
-// console.log(document);
-document.getElementsByTagName('ul').append
-const pragraph = document.getElementById('pragraph');
-console.log(pragraph);
+// console.log("hello World!");
+const sections = document.querySelectorAll('section');
+console.log(sections);
+console.log(sections.length);
