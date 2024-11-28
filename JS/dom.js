d82611aebccf38384736f1d8c208c758bf6c6915
nurnewas <nurnewasjr@gmail.com>
2024-11-28 04:30:22 -0600

    css in section by Js

--- JS/dom.js
+++ JS/dom.js
@@ -2,3 +2,9 @@
 const sections = document.querySelectorAll('section');
 console.log(sections);
 console.log(sections.length);
+
+for(const section of sections) {
+    console.log(section);
+    section.style.border ='red 3px solid';
+    section.style.margin = '10px';
+}
