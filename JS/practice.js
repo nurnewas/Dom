dc10849d09fef5ce36af0b953e1c27740096fc05
nurnewas <nurnewasjr@gmail.com>
2024-11-29 03:25:26 -0600

    addes css by js

--- JS/practice.js
+++ JS/practice.js
@@ -1,5 +1,12 @@
-const list = document.getElementsByClassName('listitem');
+const list = document.getElementsByClassName("listitem");
 
 for (const item of list) {
-    console.log(item);
+  console.log(item);
+}
+
+const sections = document.getElementsByTagName("section");
+for (const section of sections) {
+  console.log(sections);
+//   sections.style.backgroundColor = "pink";
+  section.style.border = "1px solid black";
 }
