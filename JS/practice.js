c5df58a9e0af2bbeaa31b85ade2484f32a04761e
nurnewas <nurnewasjr@gmail.com>
2024-11-29 03:35:08 -0600

    hover effect

--- JS/practice.js
+++ JS/practice.js
@@ -5,8 +5,25 @@ for (const item of list) {
 }
 
 const sections = document.getElementsByTagName("section");
+
 for (const section of sections) {
   console.log(sections);
-//   sections.style.backgroundColor = "pink";
+  section.style.backgroundColor = "pink";
   section.style.border = "1px solid black";
+  section.style.margin = "10px";
+  section.style.borderRadius = "25px 0px 25px 0px";
+
+  section.addEventListener("mouseover", () => {
+    section.style.backgroundColor = "green";
+    section.style.transition = "background-color 0.5s";
+
+    section.addEventListener("mouseout", () => {
+      section.style.backgroundColor = "pink";
+      section.style.borderRadius = "0px 25px 0px 25px ";
+    })
+  })
+
 }
+
+// Hover 
+
