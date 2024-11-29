69272284c8fe40ce88f73af4a8c86ba7e0c26ad2
nurnewas <nurnewasjr@gmail.com>
2024-11-29 11:53:30 -0600

    addEventListener

--- Js Dom Events/JS/Events.js
+++ Js Dom Events/JS/Events.js
@@ -1,5 +1,31 @@
-console.log("connection");
+// 1 Decet set on HTML 
+{/* <button onclick="document.body.style.backgroundColor = 'yellow'"></button> */}
 
-function makeRed (){
-    document.body.style.backgroundColor= 'red';
+// 2
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+// 3
+const blueMe = document.getElementById("makeMeBlue");
+blueMe.onclick = Blue;
+
+// makeMeBlue. Function
+function Blue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+
+// 3 another
+const purpleBtn = document.getElementById("purple");
+purpleBtn.onclick = function purpleColor() {
+    document.body.style.backgroundColor = "purple";
+}
+
+// 4
+const pinkBtn = document.getElementById("pink");
+pinkBtn.addEventListener("click",pinkColor);
+
+
+function pinkColor() {
+    document.body.style.backgroundColor = "pink";
 }
