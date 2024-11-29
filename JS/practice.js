649c0dbe50d362c35351ae1bf942daf503d96639
nurnewas <nurnewasjr@gmail.com>
2024-11-29 03:56:47 -0600

    adde dh the section

--- JS/practice.js
+++ JS/practice.js
@@ -13,17 +13,36 @@ for (const section of sections) {
   section.style.margin = "10px";
   section.style.borderRadius = "25px 0px 25px 0px";
 
+  // Hover
   section.addEventListener("mouseover", () => {
     section.style.backgroundColor = "green";
     section.style.transition = "background-color 0.5s";
 
     section.addEventListener("mouseout", () => {
       section.style.backgroundColor = "pink";
-      section.style.borderRadius = "0px 25px 0px 25px ";
-    })
-  })
-
+      section.style.borderRadius = "0px 25px 0px 25px";
+    });
+  });
 }
 
-// Hover 
+// add a new Element
+const second = document.getElementById("section2");
+const li = document.createElement("li");
+li.innerText = "list4";
+second.appendChild(li);
+
+// add Section
+const main = document.getElementById("main-section");
+const section = document.createElement("section");
+section.innerText = `
+<h1>This is my Dynamic section </h1>
+    <ul>
+        <li>list 1 </li>
+        <li>list 2 </li>
+        <li>list 3 </li>
+    </ul>
+`
+;
+
+main.appendChild(section);
 
