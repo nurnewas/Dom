b86b844c7e12e34c6500d4876eb27c2d78ab2302
nurnewas <nurnewasjr@gmail.com>
2024-11-30 11:41:22 -0600

    input Field to text

--- Js Dom Events/JS/practice.js
+++ Js Dom Events/JS/practice.js
@@ -15,3 +15,13 @@ document.getElementById('btnListener').addEventListener('click',function (){
    const handelStatus = document.getElementById('text');
    handelStatus.innerText = "Updated by the event status update"
 });
+
+// input field set up 
+document.getElementById('btn-update').addEventListener('click',function() {
+    const inputfield = document.getElementById('input-field');
+    // console.log(inputfield.value);
+    const inputText = inputfield.value;
+
+    const text = document.getElementById('input-text');
+    text.innerText = inputText;
+})
