fab92ccee2c6d6c12be6bdec3c37496236a5a099
nurnewas <nurnewasjr@gmail.com>
2024-11-30 03:00:23 -0600

    inner Text Change OnClick Button

---
+++ Js Dom Events/JS/practice.js
@@ -0,0 +1,11 @@
+console.log('connecting to server');
+
+function handelOnClick() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// text Change 
+function changeText() {
+    const text  = document.getElementById('text');
+    text.innerText = "Are you Hacked !!!!!"
+}
