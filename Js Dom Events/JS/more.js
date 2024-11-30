379579a7555ba57ede2d720a432268d474e15666
nurnewas <nurnewasjr@gmail.com>
2024-11-30 12:31:27 -0600

    foucs abd blur

--- Js Dom Events/JS/more.js
+++ Js Dom Events/JS/more.js
@@ -1,3 +1,10 @@
-document.getElementById("more-event").addEventListener("click", function(){
+document.getElementById("more-event").addEventListener("mousemove", function(){
     console.log("tiger");
+})
+
+document.getElementById("text").addEventListener("focus", function(){
+    console.log("cat");
+})
+document.getElementById("text").addEventListener("blur", function(){
+    console.log("dog");
 })
