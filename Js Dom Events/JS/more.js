7f86029b1bba19d1eb837715174c2c129ac1bf66
nurnewas <nurnewasjr@gmail.com>
2024-12-02 03:37:46 -0600

    useKeyUp for Get the key in console

--- Js Dom Events/JS/more.js
+++ Js Dom Events/JS/more.js
@@ -11,7 +11,22 @@ document.getElementById("text").addEventListener("blur", function () {
   console.log("dog");
 });
 
-document.getElementById("text").addEventListener("keydown", function () {
+// document.getElementById("text").addEventListener("keydown", function () {
+//   console.log(event.target.value);
+//   const key = event.target.value;
+//   document.getElementById("keys");
+//   keys.innerText = key;
+// }); //keyboard Typeahead
+
+
+// document.getElementById("text").addEventListener("keypress", function () {
+//   console.log(event.target.value);
+//   const key = event.target.value;
+//   document.getElementById("keys");
+//   keys.innerText = key;
+// }); //keyboard Typeahead
+
+document.getElementById("text").addEventListener("keyup", function () {
   console.log(event.target.value);
   const key = event.target.value;
   document.getElementById("keys");
