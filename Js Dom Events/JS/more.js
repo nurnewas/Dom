b1b6ae8b153a504d33f51a89050e439b13f2d0c5
nurnewas <nurnewasjr@gmail.com>
2024-12-02 03:33:41 -0600

    add keyboad text to p tag

--- Js Dom Events/JS/more.js
+++ Js Dom Events/JS/more.js
@@ -1,14 +1,20 @@
-document.getElementById("more-event").addEventListener("mousemove", function(){
+document
+  .getElementById("more-event")
+  .addEventListener("mousemove", function () {
     console.log("tiger");
-})
+  });
 
-document.getElementById("text").addEventListener("focus", function(){
-    console.log("cat");
-})
-document.getElementById("text").addEventListener("blur", function(){
-    console.log("dog");
-}) 
+document.getElementById("text").addEventListener("focus", function () {
+  console.log("cat");
+});
+document.getElementById("text").addEventListener("blur", function () {
+  console.log("dog");
+});
 
-document.getElementById("text").addEventListener('keydown', function(){
-    console.log(event.target.value);
+document.getElementById("text").addEventListener("keydown", function () {
+  console.log(event.target.value);
+  const key = event.target.value;
+  document.getElementById("keys");
+  keys.innerText = key;
 }); //keyboard Typeahead
+
