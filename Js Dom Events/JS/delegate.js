84a034eae581abf321cf98196a5f6008945ec74a
nurnewas <nurnewasjr@gmail.com>
2024-12-07 04:30:22 -0600

    items remove with the parent

--- Js Dom Events/JS/delegate.js
+++ Js Dom Events/JS/delegate.js
@@ -1,19 +1,24 @@
-console.log('connected to server');
+console.log("connected to server");
 
-const items = document.getElementsByClassName('item');
-for (const item of items){
-    item.addEventListener('click',function(e){
-        // console.log(item.innerHTML);
-        // console.log(e.target.parentNode);
-        e.target.parentNode.removeChild(e.target);
-    });
-};
+// const items = document.getElementsByClassName('item');
+// for (const item of items){
+//     item.addEventListener('click',function(e){
+//         // console.log(item.innerHTML);
+//         // console.log(e.target.parentNode);
+//         e.target.parentNode.removeChild(e.target);
+//     });
+// };
+document
+  .getElementById("content-container")
+  .addEventListener("click", function (e) {
+    e.target.parentNode.removeChild(e.target);
+  });
 
-// add  button 
-document.getElementById('btn-add-item').addEventListener('click',function(){
-    const container = document.createElement('content-container');
+// add  button
+document.getElementById("btn-add-item").addEventListener("click", function () {
+  const container = document.createElement("content-container");
 
-    const li = document.createElement('li');
-    li.textContent = 'New Item';
-    container.appendChild(li);
+  const li = document.createElement("li");
+  li.textContent = "New Item";
+  container.appendChild(li);
 });
